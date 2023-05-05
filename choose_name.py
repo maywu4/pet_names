759ab6edd6f0bbf49342f405f241a15cad1cce99
May Wu <maywu4@gmail.com>
2023-05-05 16:14:23 -0400

    add a name and re-select

--- choose_name.py
+++ choose_name.py
@@ -8,5 +8,28 @@ selected_name = random.choice(names_list)
 
 # print(names)
 # print(names_list)
+# print(selected_name)
+
+
+#add name
+file.close()
+
+file = open('petnames.txt', 'a')
+
+file.writelines('\nMaxi')
+
+file.close()
+
+
+#re-select
+file = open('petnames.txt', 'r')
+
+names = file.read()
+names_list = names.split("\n")
+last_name = names_list[-1]
+
+# print(names)
+# print(names_list)
 print(selected_name)
+print(last_name)
 
